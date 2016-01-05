5e63ce752f34f7a317d9c9c90ee44a12c9c2a110
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2016-01-05 19:53:00 +0200

    [Student] Added code to read CSV thanks to a JSON schema.

--- student/src/main/scala/com/amadeus/ti/induction/Introduction.scala
+++ student/src/main/scala/com/amadeus/ti/induction/Introduction.scala
@@ -1,5 +1,7 @@
 package com.amadeus.ti.induction
 
+//import org.apache.spark.sql.types._
+
 //
 object Introduction extends App {
 
@@ -36,5 +38,91 @@ object Introduction extends App {
     //
     rddOfStudents
   }
+
+  // //////////// Second way: from JSON schema //////////////
+  println ("/////////// Second way: from JSON schema /////////////")
+
+  // From the local filesystem
+  val schemaFilepath = "data/schema/profiles.json"
+  // From HDFS
+  // val schemaFilepath = "hdfs://localhost:9000/data/scalada/profiles.json"
+  val dFrame : org.apache.spark.sql.DataFrame =
+    sqlContext.read.json (schemaFilepath)
+
+  // DEBUG
+  dFrame.printSchema()
+  dFrame.show()
+
+  // Using JSONRDD
+  val strRDD : org.apache.spark.rdd.RDD[String] =
+    sparkContext.textFile (schemaFilepath)
+  val jsonRDD : org.apache.spark.sql.DataFrame = sqlContext.read.json (strRDD)
+
+  // DEBUG
+  jsonRDD.printSchema()
+  jsonRDD.show()
+
+  // Explicit Schema Definition
+  val profilesSchema = org.apache.spark.sql.types.StructType (
+    Seq (
+      org.apache.spark.sql.types.StructField ("id",
+        org.apache.spark.sql.types.StringType, true),
+      org.apache.spark.sql.types.StructField ("about",
+        org.apache.spark.sql.types.StringType, true),
+      org.apache.spark.sql.types.StructField ("address",
+        org.apache.spark.sql.types.StringType, true),
+      org.apache.spark.sql.types.StructField ("age",
+        org.apache.spark.sql.types.IntegerType, true),
+      org.apache.spark.sql.types.StructField ("company",
+        org.apache.spark.sql.types.StringType, true),
+      org.apache.spark.sql.types.StructField ("email",
+        org.apache.spark.sql.types.StringType, true),
+      org.apache.spark.sql.types.StructField ("eyeColor",
+        org.apache.spark.sql.types.StringType, true),
+      org.apache.spark.sql.types.StructField ("favoriteFruit",
+        org.apache.spark.sql.types.StringType, true),
+      org.apache.spark.sql.types.StructField ("gender",
+        org.apache.spark.sql.types.StringType, true),
+      org.apache.spark.sql.types.StructField ("name",
+        org.apache.spark.sql.types.StringType, true),
+      org.apache.spark.sql.types.StructField ("phone",
+        org.apache.spark.sql.types.StringType, true),
+      org.apache.spark.sql.types.StructField ("registered",
+        org.apache.spark.sql.types.TimestampType, true),
+      org.apache.spark.sql.types.StructField ("tags",
+        org.apache.spark.sql.types.ArrayType (org.apache.spark.sql.types.StringType), true)
+    )
+  )
+
+  val jsonRDDWithSchema : org.apache.spark.sql.DataFrame =
+    sqlContext.jsonRDD (strRDD, profilesSchema)
+
+  // DEBUG
+  jsonRDDWithSchema.printSchema()
+  jsonRDDWithSchema.show()
+
+  //
+  jsonRDDWithSchema.registerTempTable ("profilesTable")
+
+  // Filter based on timestamp
+  val filterCount = sqlContext.sql ("select * from profilesTable where registered> CAST('2014-08-26 00:00:00' AS TIMESTAMP)").count
+
+  val fullCount = sqlContext.sql ("select * from profilesTable").count
+
+  println ("All Records Count : " + fullCount) //200
+  println ("Filtered based on timestamp count : " + filterCount) //106
+
+  // Write schema as JSON to file
+  scala.reflect.io.File ("profileSchema.json").writeAll (profilesSchema.json)
+
+  val loadedSchema =
+    org.apache.spark.sql.types.DataType.fromJson (
+      scala.io.Source
+        .fromFile ("profileSchema.json")
+        .mkString
+    )
+
+  // Print loaded schema
+  println (loadedSchema.prettyJson)
 
 }
