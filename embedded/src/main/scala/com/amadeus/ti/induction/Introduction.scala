00fd60c33494d1bf99ebfb80cd0e133e0ae6b90b
Denis Arnaud <denis.arnaud_fedora@m4x.org>
2017-05-17 15:19:34 +0300

    [Embedded] Fixed the file-path issue

--- embedded/src/main/scala/com/amadeus/ti/induction/Introduction.scala
+++ embedded/src/main/scala/com/amadeus/ti/induction/Introduction.scala
@@ -47,7 +47,8 @@ object Introduction extends App {
   // val dataFilepath = "data/profiles.json"
   // JSON data file, from HDFS
   // (check the fs.default.name property in the /etc/hadoop/core-site.xml file)
-  val dataFilepath= "hdfs://172.30.1.129:8020/data/induction/embedded/profiles.json"
+  //val dataFilepath = "data/profiles.json"
+  val dataFilepath = "hdfs://172.30.1.129:8020/user/darnaud/data/induction/embedded/profiles.json"
   val dFrame : org.apache.spark.sql.DataFrame =
     sqlContext.read.json (dataFilepath)
 
